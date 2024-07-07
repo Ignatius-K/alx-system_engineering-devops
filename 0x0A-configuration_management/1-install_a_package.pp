4731c84ff25adab3af756f2750da577d9ed01fea
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-07 11:14:00 +0300

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
 # Manifest install `flask` package
+# package { 'flask':
+#   ensure   => '2.1.0',
+#   provider => pip3,
+# }
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => pip3,
+exec { 'Install Flask':
+  command => '/usr/bin/pip3 install flask==2.1.0'
 }
