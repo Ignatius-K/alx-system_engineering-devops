e316f5676c4b6a2736f945948b068479126ff335
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-07-09 00:33:33 +0300

    2. Add a custom HTTP header with Puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,8 @@
+# Manifest adds a Custom Response Header
+exec { 'Add Header':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $hostname;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
+}
