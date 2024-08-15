819d978725fb94a7450ce78b52ac4c0ab059ae25
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-08-16 01:52:28 +0300

    0. How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,39 +1,51 @@
 #!/usr/bin/python3
 """API advanced development"""
 
-from requests import Request, Session
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """Get the number of subscribers
+    """Gets subscribers on given reddit"""
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    headers = {
+        "User-Agent": "linux:5.0 (by /u/natius)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        results = response.json().get("data")
+        return results.get("subscribers")
+    return 0
 
-    Queries the Redit endpoint to get subscribers
-    of particular subreddit
+# def number_of_subscribers(subreddit):
+#     """Get the number of subscribers
 
-    Return:
-        (int): The number of subscribers, 0 if invalid subreddit
-    """
-    request = Request(
-        method='GET',
-        url='https://www.reddit.com/subreddits/search.json',
-        params={
-            'q': subreddit
-        },
-    )
+#     Queries the Redit endpoint to get subscribers
+#     of particular subreddit
 
-    session = Session()
-    session.headers.update({
-        'User-agent': 'Linux/5.4.0 MyRedditApp/2.3 (by /u/natius-k)'
-    })
-    preparedrequest = session.prepare_request(request)
-    subscribers = 0
+#     Return:
+#         (int): The number of subscribers, 0 if invalid subreddit
+#     """
+#     request = Request(
+#         method='GET',
+#         url='https://www.reddit.com/subreddits/search.json',
+#         params={
+#             'q': subreddit
+#         },
+#     )
 
-    try:
-        response = session.send(preparedrequest, allow_redirects=False)
-        response.raise_for_status()
-        response = response.json()
-        data = response.get('data').get('children')[0].get('data')
-        subscribers = data.get('subscribers')
-    except Exception as e:
-        pass
-    return subscribers
+#     session = Session()
+#     session.headers.update({
+#         'User-agent': 'Linux/5.4.0 MyRedditApp/2.3 (by /u/natius-k)'
+#     })
+#     preparedrequest = session.prepare_request(request)
+#     subscribers = 0
+
+#     try:
+#         response = session.send(preparedrequest, allow_redirects=False)
+#         response.raise_for_status()
+#         response = response.json()
+#         data = response.get('data').get('children')[0].get('data')
+#         subscribers = data.get('subscribers')
+#     except Exception as e:
+#         pass
+#     return subscribers
